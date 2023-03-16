efdf850e58ece986ed5fd067d13129dbfaf14055
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-16 22:25:27 +0530

    Update ToDoItem.jsx

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,1 +1,34 @@
 
+import React, { useState } from "react";
+
+function ToDoItem(props) {
+  // const [iD, sId] = useState(false);
+
+  // function hC() {
+  //   sId((prevValue) => {
+  //     return !prevValue;
+  //   });
+  // }
+
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+
+        // task ta jokhun ei click hocche
+        // unChecked bole key/variable  ta trigger hocche
+        // Unchecked ke trigger korchi amra with the index of the task
+        // mane task r index take amra send korchi unchecked ke
+        //
+        //
+        //
+      }}
+    >
+      {/* <li style={{ textDecoration: iD ? "line-through" : "none" }}>
+        {props.task}
+      </li> */}
+      <li>{props.tasks}</li>
+    </div>
+  );
+}
+export default ToDoItem;
